f7d21e113a10d7109c9209862977947b144af6b6
Emmanouil Konstantinidis <hello@manos.im>
2020-12-21 16:34:33 +0000

    chore: Write tests for build command

--- src/cli/build.ts
+++ src/cli/build.ts
@@ -1,20 +1,21 @@
-import { execSync } from 'child_process';
+import cp from 'child_process';
 
 import { getConfig } from './config';
 import { BuildRunOptions, Config } from './types';
 
-const buildIOS = async (config: Config): Promise<void> => {
+export const buildIOS = async (config: Config): Promise<void> => {
   const buildCommand =
     config.ios?.buildCommand ||
     `xcodebuild -workspace ${config.ios?.workspace} -scheme ${config.ios?.scheme} -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -quiet`;
-  execSync(buildCommand, { stdio: 'inherit' });
+
+  cp.execSync(buildCommand, { stdio: 'inherit' });
 };
 
-const buildAndroid = async (config: Config): Promise<void> => {
+export const buildAndroid = async (config: Config): Promise<void> => {
   const buildCommand =
     config.android?.buildCommand ||
     `cd android/ && ./gradlew assembleDebug && cd -`;
-  execSync(buildCommand, { stdio: 'inherit' });
+  cp.execSync(buildCommand, { stdio: 'inherit' });
 };
 
 export const buildHandler = async (args: BuildRunOptions) => {
