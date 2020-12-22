b5d8914916e3c02d7128b3eacf47e874b495acd2
Emmanouil Konstantinidis <hello@manos.im>
2020-12-22 12:19:32 +0000

    chore: Add JSDoc descriptions to types

--- src/cli/types.ts
+++ src/cli/types.ts
@@ -6,19 +6,25 @@ export interface BuildRunOptions extends Arguments {
 }
 
 type ConfigIOS = {
+  /** The workspace to build */
   workspace?: string;
+  /** The scheme to build */
   scheme?: string;
+  /** Overrides the `xcodebuild` command making the workspace & scheme options obselete. */
   buildCommand?: string;
+  /** Passes the quiet flag to `xcodebuild` */
   quiet?: boolean;
 };
 
 type ConfigAndroid = {
+  /** Overrides the `assembleDebug` gradle command. Should build the apk. */
   buildCommand?: string;
 };
 
 export type Config = {
   ios?: ConfigIOS;
   android?: ConfigAndroid;
+  /** Prevents the CLI/library from printing any logs/output*/
   debug?: boolean;
 };
 
