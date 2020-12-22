c1caa82ba5f1437ce9930c01ea062cb074d9c8d3
Emmanouil Konstantinidis <hello@manos.im>
2020-12-22 21:19:33 +0000

    feat: Build iOS - uninstall, install and launch on simulator

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -1,5 +1,56 @@
-import { BuildRunOptions } from './types';
+import path from 'path';
+import execa from 'execa';
+
+import { BuildRunOptions, Config, Logger } from './types';
+import { createLogger } from '../logger';
+import { getConfig } from './config';
+
+const getIOSBundleIdentifier = (appPath: string): string => {
+  const { stdout } = execa.commandSync(
+    `mdls -name kMDItemCFBundleIdentifier -r ${appPath}`
+  );
+  return stdout;
+};
+
+export const runIOS = async (config: Config, logger: Logger) => {
+  const DEFAULT_BINARY_DIR = '/ios/build/Build/Products/Debug-iphonesimulator';
+  const cwd = path.join(
+    process.cwd(),
+    config.ios?.binaryPath
+      ? path.dirname(config.ios?.binaryPath)
+      : DEFAULT_BINARY_DIR
+  );
+
+  const appFilename = config.ios?.binaryPath
+    ? path.basename(config.ios?.binaryPath)
+    : `${config.ios?.scheme}.app`;
+  const appPath = path.join(cwd, appFilename);
+  const bundleId = getIOSBundleIdentifier(appPath);
+  const simulator = config.ios?.device.replace(/([ /])/g, '\\$1');
+
+  // Uninstall
+  const uninstallCommand = `xcrun simctl uninstall ${simulator} ${bundleId}`;
+  await execa.command(uninstallCommand, { stdio: 'inherit', cwd });
+
+  // Install
+  const installCommand = `xcrun simctl install ${simulator} ${appFilename}`;
+  await execa.command(installCommand, { stdio: 'inherit', cwd });
+
+  // Launch
+  const launchCommand = `xcrun simctl launch ${simulator} ${bundleId}`;
+  await execa.command(launchCommand, { stdio: 'inherit', cwd });
+};
+
+export const runAndroid = async (config: Config, logger: Logger) => {
+  // Coming Soon
+};
 
 export const runHandler = async (args: BuildRunOptions) => {
-  console.log(`OWL will build for the ${args.platform} platform.`);
+  const config = await getConfig(args.config);
+  const logger = createLogger(config.debug);
+  const runProject = args.platform === 'ios' ? runIOS : runAndroid;
+
+  await runProject(config, logger);
+
+  logger.info(`OWL will run the app on ${args.platform}.`);
 };
