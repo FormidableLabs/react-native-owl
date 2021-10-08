4c38585a22254fb1d4816a1a9b3286a979d9d3aa
Manos Konstantinidis <hello@manos.im>
2021-10-08 11:32:57 +0100

    Store screenshot using the specified filename

--- example/.prettierrc.js
+++ example/.prettierrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
 };
--- example/__tests__/App.owl.tsx
+++ example/__tests__/App.owl.tsx
@@ -1,8 +1,8 @@
-import {takeScreenshot} from 'react-native-owl';
+import { takeScreenshot } from 'react-native-owl';
 
 describe('App.tsx', () => {
   it('takes a screenshot of the first screen', async () => {
-    const screen = await takeScreenshot();
+    const screen = await takeScreenshot('homescreen');
 
     expect(screen).toMatchBaseline();
   });
