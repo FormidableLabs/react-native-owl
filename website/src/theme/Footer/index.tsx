83f08f09f1afcd0c270640f794958b75008c3ec4
Manos Konstantinidis <hello@manos.im>
2022-01-19 22:32:05 +0000

    chore: Add footer text

--- website/src/theme/Footer/index.tsx
+++ website/src/theme/Footer/index.tsx
@@ -14,37 +14,46 @@ export default function Footer() {
   return (
     <footer className="footer footer--dark">
       <div className="container container--fluid">
-        <a href="https://formidable.com">
-          <FormidableLogo className={styles.footerLogo} />
-        </a>
-      </div>
+        <div className={styles.footerContent}>
+          <div className={styles.logoLinksContainer}>
+            <a href="https://formidable.com">
+              <FormidableLogo className={styles.footerLogo} />
+            </a>
 
-      <div className="container container--fluid">
-        {links && links.length > 0 && (
-          <div className="footer__links">
-            {links.map((linkItem, i) => (
-              <div key={i} className="margin-vert--md">
-                {linkItem.title && (
-                  <h4 className="footer__title">{linkItem.title}</h4>
-                )}
-                {linkItem.items != null &&
-                Array.isArray(linkItem.items) &&
-                linkItem.items.length > 0
-                  ? linkItem.items.map((item, key) => (
-                      <span key={item.to}>
-                        <Link className="footer__link-item" to={item.to}>
-                          {item.label}
-                        </Link>
-                        {key !== linkItem.items.length - 1 && (
-                          <span className="footer__link-separator">·</span>
-                        )}
-                      </span>
-                    ))
-                  : null}
+            {links && links.length > 0 && (
+              <div className="footer__links">
+                {links.map((linkItem, i) => (
+                  <div key={i} className="margin-vert--md">
+                    {linkItem.title && (
+                      <h4 className="footer__title">{linkItem.title}</h4>
+                    )}
+                    {linkItem.items != null &&
+                    Array.isArray(linkItem.items) &&
+                    linkItem.items.length > 0
+                      ? linkItem.items.map((item, key) => (
+                          <span key={item.to}>
+                            <Link className="footer__link-item" to={item.to}>
+                              {item.label}
+                            </Link>
+                            {key !== linkItem.items.length - 1 && (
+                              <span className="footer__link-separator">·</span>
+                            )}
+                          </span>
+                        ))
+                      : null}
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
-        )}
+
+          <div className={styles.footerDescription}>
+            Formidable is a global design and engineering consultancy and open
+            source software organization, specializing in digital products and
+            transformation. We have location hubs in Seattle, London, Toronto,
+            Denver, Atlanta, and Phoenix with remote consultants worldwide.
+          </div>
+        </div>
       </div>
 
       <div className="container container--fluid">
