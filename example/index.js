7fdf75260c2692875110bde64af8f6f1660e3087
Alessandro Senese <alessandro.senese@formidable.com>
2022-03-16 16:27:43 +0000

    fix import

--- example/index.js
+++ example/index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { initClient } from 'react-native-owl/dist/client';
+
+initClient();
 
 AppRegistry.registerComponent(appName, () => App);
