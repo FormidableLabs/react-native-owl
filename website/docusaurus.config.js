eaa196d6dd4c4974b06f6dd9aad7b71eba258c0c
Grant Sander <gksander93@gmail.com>
2023-05-11 08:42:19 -0500

    Vercel docs deploy (#153)
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * Remove surge
    
    * homepage image urls
    
    * Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'React Native Owl',
   tagline: 'Visual Regression Testing for React Native',
   url: 'https://formidable.com',
-  baseUrl: '/open-source/react-native-owl/',
+  baseUrl: process.env.VERCEL_ENV === "preview" ? '/' : '/open-source/react-native-owl/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'images/favicon.ico',
